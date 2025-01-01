96c3befc013ae3cc2a29c090ca49a87f0d2451cb
shufps <shufps80@gmail.com>
2025-01-01 19:27:23 +0100

    nerd eko support

--- main/main.cpp
+++ main/main.cpp
@@ -17,6 +17,7 @@
 #include "boards/nerdaxe.h"
 #include "boards/nerdaxegamma.h"
 #include "boards/nerdhaxegamma.h"
+#include "boards/nerdeko.h"
 #include "create_jobs_task.h"
 #include "global_state.h"
 #include "history.h"
@@ -155,6 +156,8 @@ extern "C" void app_main(void)
 #endif
 #ifdef NERDHAXEGAMMA
     Board *board = new NerdHaxeGamma();
+#ifdef NERDEKO
+    Board *board = new NerdEko();
 #endif
 
     // initialize everything non-asic-specific like
