0b93390feb8e80462b5de71d88b08bb92ed7fe11
WantClue <Lars_Naether@hotmail.de>
2025-02-08 18:00:41 +0100

    fix: field order

--- main/displays/APIs.cpp
+++ main/displays/APIs.cpp
@@ -77,8 +77,8 @@ unsigned int getBTCprice(void)
         esp_http_client_config_t config = {
             .url = getBTCAPI,
             .event_handler = http_event_handler,
-            .skip_cert_common_name_check = true,
-            .cert_pem = NULL
+            .cert_pem = NULL,
+            .skip_cert_common_name_check = true
         };
 
         esp_http_client_handle_t client = esp_http_client_init(&config);
