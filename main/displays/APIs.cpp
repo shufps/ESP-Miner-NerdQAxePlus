0bc154a9f1c0c7e0820ae6628ea08d32a7073d7d
WantClue <Lars_Naether@hotmail.de>
2025-02-10 00:46:57 +0100

    fix: update order structure

--- main/displays/APIs.cpp
+++ main/displays/APIs.cpp
@@ -116,13 +116,11 @@ esp_err_t http_event_handler(esp_http_client_event_t *evt)
 
 unsigned int getBTCprice(void)
 {
-    static char price_str[32];
-
     if ((mBTCUpdate == 0) || (esp_timer_get_time() / 1000 - mBTCUpdate > UPDATE_BTC_min * 60)) {
         esp_http_client_config_t config = {
             .url = getBTCAPI,
-            .event_handler = http_event_handler,
             .cert_pem = root_ca_certificate,
+            .event_handler = http_event_handler,
             .skip_cert_common_name_check = true
         };
 
