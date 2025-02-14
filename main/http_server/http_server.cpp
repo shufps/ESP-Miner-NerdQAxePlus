60b3a89064eecc0605cb227fab5bc5471c7ba685
shufps <shufps80@gmail.com>
2025-02-14 22:08:14 +0100

    added free()

--- main/http_server/http_server.cpp
+++ main/http_server/http_server.cpp
@@ -828,9 +828,6 @@ static esp_err_t POST_WWW_update(httpd_req_t *req)
         return httpd_resp_send_err(req, HTTPD_401_UNAUTHORIZED, "Unauthorized");
     }
 
-    // don't put it on the stack
-    char *buf = (char*) malloc(2048);
-
     int remaining = req->content_len;
 
     const esp_partition_t *www_partition =
@@ -849,6 +846,9 @@ static esp_err_t POST_WWW_update(httpd_req_t *req)
     // Erase the entire www partition before writing
     ESP_ERROR_CHECK(esp_partition_erase_range(www_partition, 0, www_partition->size));
 
+    // don't put it on the stack
+    char *buf = (char*) malloc(2048);
+
     while (remaining > 0) {
         int recv_len = httpd_req_recv(req, buf, MIN(remaining, 2048));
 
@@ -856,16 +856,20 @@ static esp_err_t POST_WWW_update(httpd_req_t *req)
             continue;
         } else if (recv_len <= 0) {
             httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Protocol Error");
+            free(buf);
             return ESP_FAIL;
         }
 
         if (esp_partition_write(www_partition, www_partition->size - remaining, (const void *) buf, recv_len) != ESP_OK) {
             httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Write Error");
+            free(buf);
             return ESP_FAIL;
         }
 
         remaining -= recv_len;
     }
+
+    free(buf);
 
     httpd_resp_sendstr(req, "WWW update complete\n");
     return ESP_OK;
@@ -880,14 +884,14 @@ static esp_err_t POST_OTA_update(httpd_req_t *req)
         return httpd_resp_send_err(req, HTTPD_401_UNAUTHORIZED, "Unauthorized");
     }
 
-    // don't put it on the stack
-    char *buf = (char*) malloc(2048);
-
     esp_ota_handle_t ota_handle;
     int remaining = req->content_len;
 
     const esp_partition_t *ota_partition = esp_ota_get_next_update_partition(NULL);
     ESP_ERROR_CHECK(esp_ota_begin(ota_partition, OTA_SIZE_UNKNOWN, &ota_handle));
+
+    // don't put it on the stack
+    char *buf = (char*) malloc(2048);
 
     while (remaining > 0) {
         int recv_len = httpd_req_recv(req, buf, MIN(remaining, 2048));
@@ -899,6 +903,7 @@ static esp_err_t POST_OTA_update(httpd_req_t *req)
             // Serious Error: Abort OTA
         } else if (recv_len <= 0) {
             httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Protocol Error");
+            free(buf);
             return ESP_FAIL;
         }
 
@@ -906,11 +911,14 @@ static esp_err_t POST_OTA_update(httpd_req_t *req)
         if (esp_ota_write(ota_handle, (const void *) buf, recv_len) != ESP_OK) {
             esp_ota_abort(ota_handle);
             httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Flash Error");
+            free(buf);
             return ESP_FAIL;
         }
 
         remaining -= recv_len;
     }
+
+    free(buf);
 
     // Validate and switch to new OTA image and reboot
     if (esp_ota_end(ota_handle) != ESP_OK || esp_ota_set_boot_partition(ota_partition) != ESP_OK) {
