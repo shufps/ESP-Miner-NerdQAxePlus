330cd3c975414a5cc81debbf48f958ef476ddb09
shufps <shufps80@gmail.com>
2025-03-06 18:42:15 +0100

    initvoltagemillis

--- main/boards/nerdeko.cpp
+++ main/boards/nerdeko.cpp
@@ -9,7 +9,7 @@ NerdEko::NerdEko() : NerdQaxePlus2() {
     m_asicModel   = "BM1370";
     m_version     = 100;
     m_numPhases   = 6;
-    m_initVoltage = 0.0;    // use m_asicVoltage for init
+    m_initVoltageMillis = 0;    // use m_asicVoltage for init
 
     m_maxVin = 13.0;
     m_minVin = 8.0;
@@ -25,25 +25,25 @@ NerdEko::NerdEko() : NerdQaxePlus2() {
 
     // m_asicCount         = 9;
     // m_maxPin            = 300.0;
-    // m_minPin            = 30.0;    
+    // m_minPin            = 30.0;
     // m_imax              = 180;  // change R14 : 33K2
-    // m_ifault            = 150.0;    
+    // m_ifault            = 150.0;
     // m_asicMaxDifficulty = 4096;
     // m_asicMinDifficulty = 1024;
 
     // m_asicCount         = 6;
     // m_maxPin            = 200.0;
-    // m_minPin            = 30.0;    
+    // m_minPin            = 30.0;
     // m_imax              = 120;  // change R14
-    // m_ifault            = 120.0;    
+    // m_ifault            = 120.0;
     // m_asicMaxDifficulty = 2048;
     // m_asicMinDifficulty = 512;
 
     m_asicCount         = 3;
     // m_maxPin            = 100.0;
-    // m_minPin            = 30.0;    
+    // m_minPin            = 30.0;
     // m_imax              = 70;   // change R14
-    m_ifault            = 60.0;    
+    m_ifault            = 60.0;
     m_asicMaxDifficulty = 1024;
     m_asicMinDifficulty = 256;
 
