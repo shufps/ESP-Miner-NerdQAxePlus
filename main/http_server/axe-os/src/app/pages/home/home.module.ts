fb0b1da22a034fc4a3984e59f549233606d60abe
shufps <shufps80@gmail.com>
2025-04-18 14:42:41 +0200

    added badge which pool is active and re-added lost links to pool stats

--- main/http_server/axe-os/src/app/pages/home/home.module.ts
+++ main/http_server/axe-os/src/app/pages/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { NbCardModule, NbButtonModule, NbThemeModule, NbIconModule } from '@nebular/theme';
+import { NbCardModule, NbBadgeModule, NbButtonModule, NbThemeModule, NbIconModule } from '@nebular/theme';
 import { SystemService } from '../../services/system.service';
 import { Chart } from 'chart.js/auto'; // Ensure Chart.js is properly imported
 import { GaugeModule } from '../../components/gauge/gauge.module';
+import { PipesModule} from '../../pipes/pipes.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { GaugeModule } from '../../components/gauge/gauge.module';
     NbButtonModule,
     NbIconModule,
     NbThemeModule,
-    GaugeModule
+    GaugeModule,
+    PipesModule,
+    NbBadgeModule,
   ],
   providers: [
     SystemService
