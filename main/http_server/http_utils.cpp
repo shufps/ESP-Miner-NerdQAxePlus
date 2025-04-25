39cbe2789780694697e520ac6d3bb0a42e18f816
shufps <shufps80@gmail.com>
2025-04-25 21:00:25 +0200

    use MALLOC and FREE macros

--- main/http_server/http_utils.cpp
+++ main/http_server/http_utils.cpp
@@ -11,7 +11,7 @@ esp_err_t sendJsonResponse(httpd_req_t *req, JsonDocument &doc) {
     // Measure the size needed for the JSON text
     size_t jsonLength = measureJson(doc);
     // Allocate a buffer from PSRAM (or regular heap if you prefer)
-    char *jsonOutput = (char*) heap_caps_malloc(jsonLength + 1, MALLOC_CAP_SPIRAM);
+    char *jsonOutput = (char*) MALLOC(jsonLength + 1);
     if (!jsonOutput) {
         ESP_LOGE(TAG, "Failed to allocate memory for JSON output");
         httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Memory allocation error");
@@ -22,7 +22,7 @@ esp_err_t sendJsonResponse(httpd_req_t *req, JsonDocument &doc) {
     // Send the response
     esp_err_t ret = httpd_resp_sendstr(req, jsonOutput);
     // Free the allocated buffer
-    heap_caps_free(jsonOutput);
+    FREE(jsonOutput);
     return ret;
 }
 
