816c03a3b8a89e7b2437ca28889778e5ce55179f
shufps <shufps80@gmail.com>
2025-10-16 10:28:21 +0200

    added missing "#"

--- main/boards/nerdqx.cpp
+++ main/boards/nerdqx.cpp
@@ -66,7 +66,7 @@ NerdQX::NerdQX() : NerdQaxePlus2() {
     m_theme = new ThemeNerdqx();
 #endif
 
-    m_swarmColorName = "7300e7";
+    m_swarmColorName = "#7300e7";
     m_defaultTheme = "default"; // light theme
     m_vrFrequency = m_defaultVrFrequency = 35000;
 }
