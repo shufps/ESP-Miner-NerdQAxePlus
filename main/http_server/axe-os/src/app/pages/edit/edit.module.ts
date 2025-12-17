c0f86a40475875a310b43336dc9ea0a5ee38eaab
Pmaxsd <pmaxsd80@gmail.com>
2025-12-17 15:13:52 +0100

    added ui swap button

--- main/http_server/axe-os/src/app/pages/edit/edit.module.ts
+++ main/http_server/axe-os/src/app/pages/edit/edit.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NbCardModule, NbIconModule, NbButtonModule, NbInputModule, NbSelectModule, NbCheckboxModule, NbTabsetModule/*, NbSliderModule*/ } from '@nebular/theme';
+import { NbCardModule, NbTooltipModule, NbIconModule, NbButtonModule, NbInputModule, NbSelectModule, NbCheckboxModule, NbTabsetModule/*, NbSliderModule*/ } from '@nebular/theme';
 import { EditComponent } from './edit.component';
 import { AdvancedToggleModule } from '../../components/advanced-toggle/advanced-toggle.module';
 import { FormsModule } from '@angular/forms';
@@ -24,6 +24,7 @@ import { AlertBannerModule } from 'src/app/components/alert-banner/alert-banner.
     FormsModule,
     TranslateModule,
     AlertBannerModule,
+    NbTooltipModule,
   ],
   exports: [EditComponent]
 })
