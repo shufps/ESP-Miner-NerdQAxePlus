e5d42d73ae4deb1c7c3f7944c2b282bfea0da3c7
Pmaxsd <pmaxsd80@gmail.com>
2025-12-17 21:32:45 +0100

    changed public pool default port

--- main/http_server/axe-os/src/app/services/system.service.ts
+++ main/http_server/axe-os/src/app/services/system.service.ts
@@ -48,7 +48,7 @@ const defaultInfo: ISystemInfo = {
   ASICModel: eASICModel.BM1368,
   deviceModel: "NerdQAxe+",
   stratumURL: "public-pool.io",
-  stratumPort: 21496,
+  stratumPort: 3333,
   stratumUser: "bc1q99n3pu025yyu0jlywpmwzalyhm36tg5u37w20d.bitaxe-U1",
   stratumEnonceSubscribe: 0,
   stratumTLS: 0,
